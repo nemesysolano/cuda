f8882a56b5dabeea8b00066749fdab3abc0da3d5
nemesysolano <nemesysolano@gmail.com>
2017-03-12 22:37:26 -0400

    Created object-pool.* sources

---
+++ object-pool/object-pool.cpp
@@ -0,0 +1,1 @@
+#include "object-pool.h"
---
+++ object-pool/object-pool.h
@@ -0,0 +1,14 @@
+/*
+ * object-pool.h
+ *
+ *  Created on: Mar 12, 2017
+ *      Author: rsolano
+ */
+
+#ifndef OBJECT_POOL_H_
+#define OBJECT_POOL_H_
+
+namespace pool {
+}
+
+#endif /* OBJECT_POOL_H_ */
