00d7dc1adadd63ce879d312da7c528ce102560bc
nemesysolano <nemesysolano@gmail.com>
2017-03-14 18:17:48 -0400

    Added Pool<LinearSolver> parameter to constructor.

--- indicator/autoregresive.cpp
+++ indicator/autoregresive.cpp
@@ -3,12 +3,3 @@
 using namespace indicator;
 using namespace timeseries;
 
-/** Creates the internal buffer that represents the lookback period.
- *
- */
-PartialAutocorrelation::PartialAutocorrelation(
-	unsigned length, /**< Lookback period's length. */
-	timeseries::TimeSeries & series /**< Bag containing the timeseries references that feed this indicator. */
-): Indicator(length, TimeSeriesBag(series)){
-
-}
--- indicator/autoregresive.h
+++ indicator/autoregresive.h
@@ -8,16 +8,22 @@
 #ifndef AUTOREGRESIVE_H_
 #define AUTOREGRESIVE_H_
 #include "indicator.h"
+#include <object-pool.h>
+#include <linear-solver.h>
 
 namespace indicator {
 	class PartialAutocorrelation: public Indicator {
+		private:
+			pool::Pool<regression::LinearSolver> & solvers_pool;
+
 		public:
 		/** Creates the internal buffer that represents the lookback period.
 		 *
 		 */
 		PartialAutocorrelation(
 			unsigned length, /**< Lookback period's length. */
-			timeseries::TimeSeries & series /**< Bag containing the timeseries references that feed this indicator. */
+			timeseries::TimeSeries & series /**< Bag containing the timeseries references that feed this indicator. */,
+			pool::Pool<regression::LinearSolver> & solvers_pool /**< Object pool that provides linear solvers to this indicator. */
 		);
 
 	};
