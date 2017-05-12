10b5f7a6640b713169a2572d8b58827163f98b23
nemesysolano <nemesysolano@gmail.com>
2017-05-11 23:16:15 -0400

    Move all function templates to header file.

--- object-pool/object-pool.h
+++ object-pool/object-pool.h
@@ -65,34 +65,144 @@ namespace pool {
 		std::mutex * life_cycle_mutex = nullptr;
 		std::thread * scheduler_thread = nullptr;
 	    std::condition_variable * condition_var = nullptr;
-	    bool * running = false;
+	    bool * running =  nullptr;
 
 		std::thread::id current_thread_id;
 		bool original = true;
 
 	public:
-
-		Pool(const Pool&);
-
 		Pool(Pool&&) = delete;
-
-		Pool & operator=(const Pool&);
 
 		Pool & operator=(Pool&&) = delete;
 
-		Pool();
+		Pool() {
+			current_thread_id = std::this_thread::get_id();
+			all = new std::list<std::shared_ptr<T>>();
+			available = new std::list<std::shared_ptr<T>>();
+			critical_section_mutex = new std::mutex();
+			life_cycle_mutex = new std::mutex();
+			scheduler_thread = new std::thread();
+		    condition_var = new std::condition_variable();
+		    running = new bool(false);
+		}
 
-		~Pool();
+		Pool(const Pool& other){
+			operator = (other);
+		}
 
-		void Add(std::shared_ptr<T> & ptr);
+		Pool<T> & operator=(const Pool& other){
+			current_thread_id = other.current_thread_id;
+			all = other.all;
+			available =other.available;
+			critical_section_mutex = other.critical_section_mutex;
+			life_cycle_mutex = other.life_cycle_mutex;
+			scheduler_thread = other.scheduler_thread;
+		    condition_var = other.condition_var;
+		    running = other.running;
 
-		void Add(std::shared_ptr<T> &&  ptr);
+		    original = false;
 
-		void Start();
+			return *this;
+		}
 
-		void Stop();
 
-		void Submit(std::function<int (const T &)>  callback) ;
+		~Pool() {
+			if(original) {
+				if(all != nullptr){
+					delete all;
+					delete available;
+					delete critical_section_mutex;
+					delete life_cycle_mutex;
+					delete scheduler_thread;
+					delete condition_var;
+					delete running;
+
+					all = nullptr;
+					available = nullptr;
+					critical_section_mutex = nullptr;
+					life_cycle_mutex = nullptr;
+					scheduler_thread = nullptr;
+					condition_var = nullptr;
+					running = nullptr;
+				}else
+					throw PoolError(POOL_ALREADY_DESTROYED, POOL_ERROR_MESSAGES[POOL_ALREADY_DESTROYED]);
+			}
+		}
+
+		void Add(std::shared_ptr<T> & ptr){
+			bool & running = *this->running;
+			if(current_thread_id != std::this_thread::get_id() || running)
+				throw PoolError(INVALID_STATE_FOR_ADDING, POOL_ERROR_MESSAGES[INVALID_STATE_FOR_ADDING]);
+
+			if(!running) {
+				this->all->push_back(ptr);
+				this->available->push_back(ptr);
+			}
+		}
+
+		void Add(std::shared_ptr<T> &&  ptr){
+			std::shared_ptr<T> & ref = ptr;
+			Add(ref);
+		}
+
+		void Start() {
+			bool & running = *this->running;
+			if(!running) {
+				std::unique_lock<std::mutex> lock(*life_cycle_mutex);
+				running = true;
+			}
+			condition_var->notify_all();
+		}
+
+		void Stop() {
+			bool & running = *this->running;
+			if(running) {
+				std::unique_lock<std::mutex> lock(*life_cycle_mutex);
+				running =false;
+			}
+			condition_var->notify_all();
+		}
+
+		void Submit(std::function<int (const T &)>  callback) {
+			bool & running = *this->running;
+			if(!running)
+				throw PoolError(INVALID_STATE_FOR_SUBMISSION, POOL_ERROR_MESSAGES[INVALID_STATE_FOR_SUBMISSION]);
+			bool found = false;
+			std::shared_ptr<T> ptr;
+
+			while(!found) {
+				{
+					std::unique_lock<std::mutex> lock(*critical_section_mutex);
+					if(this->available->size() > 0) {
+						ptr =this->available->back();
+						this->available->pop_back();
+						found = true;
+					}
+				}
+
+				if(!found){
+					std::unique_lock<std::mutex> lock(*critical_section_mutex);
+					condition_var->wait(lock);
+				}
+
+
+				if(!running)
+					break;
+			}
+
+			if(found) {
+				T * ref = ptr.get();
+				callback(*ref); //TODO: Handle exceptions in a nicely.
+
+				{
+					std::unique_lock<std::mutex> lock(*critical_section_mutex);
+					this->available->push_front (ptr);
+				}
+
+				condition_var->notify_all();
+			}
+		}
+
 
 
 	};
